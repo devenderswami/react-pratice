c9b0234f0c370bf4fd54d1bd72c450adbda16b15
Devender Swami <devenderswami@Devenders-MacBook-Pro.local>
2023-09-29 09:33:14 +0530

    igniting our app

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
-/**
- * <div id="parent">
- * <div id="child">
- * <h1>
- * I'm H1 tag
- * </h1>
- * <h2>
- * I'm h2 tag
- * </h2>
- * </div>
- * </div>
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div",{id:"parent"},
 React.createElement("div",{id:"child"}
-,[React.createElement("h1",{},"I,m an h1 tag"),
+,[React.createElement("h1",{},"this is Namaste React 🚀"),
 React.createElement("h2",{},"I,m an h2 tag")])) 
 
 console.log(parent)
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# react-pratice
-React learning 
+ # Namaste React 🚀
+
+
+ # Parcel
+ - Dev Build
+ - LocalSever
+ - HMR = Hot Module Relacement
+ - File Watching Alogrithm - written C++
+ - Caching - Faster Builds
+ - Image Optimization
+ - Minification
+ - Bundling
+ - Compress
+ - Consistent Hashing
+ - Code Splitting
+ - Differential Bundling - support older 
+ - Diagnostic
+ - Error Handling
+ - Https
+ - Tree Shaking - remove unused 
+ - Different dev and prod bundles
